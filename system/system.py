ef5e89308081fc108a29dd8d6387437cee9021ea
husvagnspc <basenmobil82@gmail.com>
2016-03-28 14:18:08 +0200

    Förbättrat system.py
    Gjort lite små justeringar

--- system/system.py
+++ system/system.py
@@ -13,10 +13,11 @@ while 1:
 	lines = file.read().splitlines()
 	file.close()
 	#Öppnar knappstatus filen
-	filename1 = "/opt/skript/system/knappstatus.txt"
+	filename1 = "/opt/skript/lampstyrning/knappstatus.txt"
 	file1 = open(filename1, "r")
 	lines1 = file1.read().splitlines()
 	file1.close()
+	#Tempgivare 1/4 Ändra: 28-011464cdc0ff denna används för tempen inne
 	tempfile = open("/sys/bus/w1/devices/28-011464cdc0ff/w1_slave")
 	thetext = tempfile.read()
 	tempfile.close()
@@ -25,6 +26,7 @@ while 1:
 	temperature = temperature / 1000
 	temperature = round(temperature, 1)
 	
+	#Tempgivare 2/4 Ändra: 28-021463284fff denna används för tempen ute
 	tempfile1 = open("/sys/bus/w1/devices/28-021463284fff/w1_slave")
 	thetext1 = tempfile1.read()
 	tempfile1.close()
@@ -33,6 +35,7 @@ while 1:
 	temperature1 = temperature1 / 1000
 	temperature1 = round(temperature1, 1)
 	
+	#Tempgivare 3/4 Ändra: 28-0314621b8cff denna används för inne i kylskåpet
 	tempfile2 = open("/sys/bus/w1/devices/28-0314621b8cff/w1_slave")
 	thetext2 = tempfile2.read()
 	tempfile2.close()
@@ -41,6 +44,8 @@ while 1:
 	temperature2 = temperature2 / 1000
 	temperature2 = round(temperature2, 1)
 
+	
+	#Tempgivare 4/4 Ändra: 28-02146335ccff denna används för kylskåps larmet
 	tempfile3 = open("/sys/bus/w1/devices/28-02146335ccff/w1_slave")
 	thetext3 = tempfile3.read()
 	tempfile3.close()
@@ -54,13 +59,19 @@ while 1:
 	print "Kyl: " ,temperature2, "grader"
 	print "Larm: ", temperature3, "grader"
 
+	#Nu avläser vi vatten nivån
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setwarnings(False)
 
+	#TRIG är på den GPIO anslutning som ljud mätaren triggar
 	TRIG = 37
+	#ECHO är på den GPIO anslutning som ljud mätaren ekar tillbaka
 	ECHO = 35
+	#Läs av i inställnings filen hur djup vattentanken är, behövs för att räkna ut mängden
 	TANK = float(lines[0])
+	#Läs av i inställnings filen när kylskåps larmet skall aktiveras
 	KYLTEMP = float(lines[1])
+	#Kolla om fläkten är aktiverad
 	FLAKTPA = float(lines1[6])
 
 	print "Cabby Vatten Kontroll Startad"
@@ -106,31 +117,32 @@ while 1:
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(36, GPIO.OUT)
 
+	#Om fläkten inte är startad manuellt så aktivera den, glöm ej att ändra relä anslutningen
 	if FLAKTPA == 0:
 		if temperature3 >=KYLTEMP:
 			GPIO.setup(36, GPIO.OUT)
 			GPIO.output(36, GPIO.LOW)
-			with open('/opt/skript/knappstatus.txt', 'r') as file:
+			with open('/opt/skript/lampstyrning/knappstatus.txt', 'r') as file:
 				data = file.readlines()
 			data[5] = '1\n'
 			data[7] = '1\n'
-			with open('/opt/skript/knappstatus.txt', 'w') as file:
+			with open('/opt/skript/lampstyrning/knappstatus.txt', 'w') as file:
 				file.writelines( data )
 			print ("Kyl Aktiverad")
 		elif temperature3 <=KYLTEMP:
 			GPIO.setup(36, GPIO.OUT)
 			GPIO.output(36, GPIO.HIGH)
-			with open('/opt/skript/knappstatus.txt', 'r') as file:
+			with open('/opt/skript/lampstyrning/knappstatus.txt', 'r') as file:
 				data = file.readlines()
 			data[5] = '0\n'
 			data[7] = '0\n'
-			with open('/opt/skript/knappstatus.txt', 'w') as file:
+			with open('/opt/skript/lampstyrning/knappstatus.txt', 'w') as file:
 				file.writelines( data )
 			print ("Kyl Avaktiverad")
 	
-	
-	if os.path.exists("/home/pi/.kodi/addons/skin.CabbyPc-touch/scripts/texten.txt"):
-		os.remove("/home/pi/.kodi/addons/skin.CabbyPc-touch/scripts/texten.txt")
+	#Vi tar bort gamla filen för att göra en ny av säkerhetsskäl innan värdena skrivs
+	if os.path.exists("/opt/skript/system/texten.txt"):
+		os.remove("/opt/skript/system/texten.txt")
 	else:
 		print("Sorry, I can not remove %s file." % filename)
 	tempen1 = str(temperature)
@@ -138,7 +150,8 @@ while 1:
 	tempen3 = str(temperature2)
 	tempen4 = str(temperature3)
 	procenten = str(procent)
-	file = open("/home/pi/.kodi/addons/skin.CabbyPc-touch/scripts/texten.txt", "w")
+	file = open("/opt/skript/system/texten.txt", "w")
+	#Skriv alla värden på separata rader 12.3 är för volten, då avläsning för det ej är inbyggt här
 	lines_of_text = [tempen1,"\n",tempen2,"\n",tempen3,"\n",procenten,"\n","12.3\n",tempen4]
 	file.writelines(lines_of_text)
 	file.close()
